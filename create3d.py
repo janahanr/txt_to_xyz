e50ad514662035f846bfc069dd07d9a20325d197
janahanr <rjanahan@gmail.com>
2021-02-04 11:13:00 -0500

    original file

---
+++ create3d.py
@@ -0,0 +1,33 @@
+import open3d
+import math
+import numpy as np
+
+pcd = open3d.io.read_point_cloud("data.xyz", format = 'xyz')
+
+print(pcd)
+
+print(np.asarray(pcd.points))
+
+po=0
+
+lines=[]
+
+for x in range(511):
+    for y in range(20):
+        if((y+1)%20==0):
+            lines.append([po+y, po])
+        else:
+            lines.append([po+y, po + y +1])
+    po += 20;
+
+po=0
+do=20
+
+for x in range(512):
+    for y in range(20):
+        lines.append([y+po, y+po+do])
+    po += 20;
+
+line_set = open3d.geometry.LineSet(points=open3d.utility.Vector3dVector(np.asarray(pcd.points)), lines = open3d.utility.Vector2iVector(lines))
+
+open3d.visualization.draw_geometries([line_set])
---
+++ txtconvet.py
@@ -0,0 +1,28 @@
+import math
+
+coord= open("data.xyz", "w") #this is the xyz file to hold the data
+data= open("dataset_400199707.txt", "r")
+
+xCtr = 0.2 * 20 #20 bc we have 20 measurements 
+xVal = 0 #keep track of x value
+
+raddiff= 360/512 * (math.pi/180)
+
+for line in data:
+    meas = line.split("\t")
+    print(meas)
+    rad= raddiff * int(meas[0])
+    i=1
+    while(xVal <= xCtr):
+        dist = int(meas[i])/1000
+        y= dist * math.cos(rad)
+        z= dist * math.sin(rad)
+        coord.write(str(-z) + " " + str(-y) + " " + str(xVal) + '\n')
+        xVal = xVal + 0.2
+        i+=1
+    xVal=0
+
+coord.close()
+data.close()
+
+
